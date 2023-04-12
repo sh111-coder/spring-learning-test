97df37fc05fbab08a598c75c9a61d313e159e38f
sh111-coder <ohk9134@naver.com>
2023-04-12 21:19:28 +0900

    feat: Spring Core DI 부분 테스트 동작하도록 구현

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,7 +1,16 @@
 package nextstep.helloworld.core.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationConstructorService {
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
+
+    @Autowired
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
