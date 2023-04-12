8c658d27905b6bcc331c040efe48de67d6e836bd
sh111-coder <ohk9134@naver.com>
2023-04-12 20:09:40 +0900

    feat: Spring MVC Exception 부분 테스트 동작하도록 구현

--- src/main/java/nextstep/helloworld/mvc/exceptions/exception/CustomException.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/exception/CustomException.java
@@ -2,4 +2,8 @@ package nextstep.helloworld.mvc.exceptions.exception;
 
 public class CustomException extends RuntimeException {
 
+    @Override
+    public String getMessage() {
+        return "CustomException";
+    }
 }
--- src/main/java/nextstep/helloworld/mvc/exceptions/exception/HelloException.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/exception/HelloException.java
@@ -2,4 +2,8 @@ package nextstep.helloworld.mvc.exceptions.exception;
 
 public class HelloException extends RuntimeException {
 
+    @Override
+    public String getMessage() {
+        return "HelloException";
+    }
 }
